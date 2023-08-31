d040ef433805b8b8e85eea76eb75f519642c762b
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-08-31 14:50:23 +0600

    card completed for ui

--- script/main.js
+++ script/main.js
@@ -1,2 +1,9 @@
-"use strict"
-// console.log('object');
+"use strict";
+/*
+All data: https://openapi.programming-hero.com/api/ai/tools
+
+Single data details: https://openapi.programming-hero.com/api/ai/tool/${id}
+
+Single data Example: https://openapi.programming-hero.com/api/ai/tool/01
+
+*/
